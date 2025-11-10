fc55bce668795dac3d05bfc1b2e908085ffa7853
hoyennhi489 <hyn489@gmail.com>
2025-11-10 21:47:33 +0700

    Limit notifications to 5 and Improve App and NotificationList styles

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,15 +1,26 @@
 import { ADD_NOTIFICATION, REMOVE_NOTIFICATION, CLEAR_ALL } from './actions';
 
 const initialState = [];
+const MAX_NOTIFICATIONS = 5;
 
 export const notificationsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_NOTIFICATION:
-      return [...state, action.payload];
+    case ADD_NOTIFICATION: {
+      const updated = [...state, action.payload];
+
+      if (updated.length > MAX_NOTIFICATIONS) {
+        updated.shift(); 
+      }
+
+      return updated;
+    }
+
     case REMOVE_NOTIFICATION:
       return state.filter((n) => n.id !== action.payload);
+
     case CLEAR_ALL:
       return [];
+
     default:
       return state;
   }
